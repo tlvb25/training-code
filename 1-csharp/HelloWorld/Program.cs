7ed19cd39dc845f49f59390fdc35ff5811315f83
tlvb25 <tlvb@msn.com>
2020-02-26 11:26:43 -0600

    add csharp directory

---
+++ 1-csharp/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using sc = System.Console; //same as "import" used in other languages which imports the whole the file,
+              //except in C# we are only linking to a file
+
+namespace HelloWorld //name variables using the pascal casing convnetion = the first letter of every word capitalized
+{ // all braces are their own line
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        sc.WriteLine("Hello World!");
+    }
+  }
+}
---
+++ 1-csharp/TestA/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TestA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        protected void Wording()
+        {
+          System.Console.WriteLine("Just a test");
+        }
+    }
+}
